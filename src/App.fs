69a8108359342a019547ac733aaaeb292ba0cc0e
ldscavo <logan@scavo.dev>
2021-02-25 17:21:33 -0500

    Refactor of header css/markup

--- src/App.fs
+++ src/App.fs
@@ -1,53 +1,40 @@
 module App
 
-(**
- The famous Increment/Decrement ported from Elm.
- You can find more info about Elmish architecture and samples at https://elmish.github.io/
-*)
-
 open Elmish
 open Elmish.React
 open Fable.React
 open Fable.React.Props
 open Fulma
 
-// MODEL
-
-type Model = int
+type Model =
+  { Count: int }
 
 type Msg =
 | Increment
 | Decrement
 
-let init() : Model = 0
-
-// UPDATE
+let init () = { Count = 1 }
 
 let update (msg:Msg) (model:Model) =
-    match msg with
-    | Increment -> model + 1
-    | Decrement -> model - 1
-
-// VIEW (rendered with React)
+  match msg with
+  | Increment -> { model with Count = model.Count + 1 }
+  | Decrement -> { model with Count = model.Count - 1 }
 
 let isMainColor = IsCustomColor "main-color"
 
 let view (model:Model) dispatch =
   Container.container [ Container.IsWideScreen ]
-      [ Navbar.navbar [ Navbar.Color isMainColor ]
-          [ Navbar.Brand.div [ Modifiers [ Modifier.TextSize (Screen.All, TextSize.Is3) ] ]
-              [ Navbar.Item.a
-                  [ Navbar.Item.Modifiers
-                      [ Modifier.TextColor IsWhite
-                        Modifier.TextWeight TextWeight.Bold ]
-                    Navbar.Item.Props [ Href "#" ] ] 
-                  [ div [ Id "logo" ] []
-                    span [ Style [ TextShadow "1px 1px #2c3e50" ] ] [ str "Gift Budget" ] ] ] ]
+    [ Navbar.navbar [ Navbar.Color isMainColor ]
+        [ Navbar.Brand.div [ Modifiers [ Modifier.TextSize (Screen.All, TextSize.Is3) ] ]
+            [ Navbar.Item.a
+                [ Navbar.Item.Props [ Id "logo-text" ;Href "#" ] ] 
+                [ div [ Id "logo" ] []
+                  span [ Style [ TextShadow "1px 1px #2c3e50" ] ] [ str "Gift Budget" ] ] ] ]
         
-        Content.content []
-          [ Button.button [ Button.OnClick (fun _ -> dispatch Increment) ] [ str "+" ]
-            div [] [ str (string model) ]
-            Button.button [ Button.OnClick (fun _ -> dispatch Decrement) ] [ str "-" ] ] ]
+      Content.content []
+        [ Button.button [ Button.OnClick (fun _ -> dispatch Increment) ] [ str "+" ]
+          div [] [ str (string model.Count) ]
+          Button.button [ Button.OnClick (fun _ -> dispatch Decrement) ] [ str "-" ] ] ]
 
 // App
 Program.mkSimple init update view
