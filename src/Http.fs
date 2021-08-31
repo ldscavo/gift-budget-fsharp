cafc7e53bb6a28233d3a4ef452a09402d2ee0621
ldscavo <logan@scavo.dev>
2021-08-30 22:39:42 -0400

    I've settled on 4 spaces for F# formatting? Please stop me if I decide to second guess this thing yet again

--- src/Http.fs
+++ src/Http.fs
@@ -6,26 +6,27 @@ open Thoth.Json
 open Model
 
 let requestOptions method (apiKey: string option) =
-  match apiKey with
-  | Some key ->
-    [ requestHeaders
-        [ Authorization (sprintf "Bearer %s" key)
-          Origin "*" ]
-      Method method ]
-  | None ->
-    [ requestHeaders [ Origin "*" ]
-      Method method ]
+    match apiKey with
+    | Some key ->
+        [ requestHeaders
+            [ Authorization (sprintf "Bearer %s" key)
+              Origin "*" ]
+          Method method ]
+    | None ->
+        [ requestHeaders [ Origin "*" ]
+          Method method ]
 
 let makeRequest method endpoint apiKey =
-  let url = sprintf "https://gifting-budget.herokuapp.com/api/%s" endpoint
-  let requestOpts = requestOptions method apiKey
+    let url = sprintf "https://gifting-budget.herokuapp.com/api/%s" endpoint
+    let requestOpts = requestOptions method apiKey
 
-  fetch url requestOpts
-  |> Promise.bind(fun response -> response.text ())
+    fetch url requestOpts
+    |> Promise.bind(fun response -> response.text ())
 
 let httpGet = makeRequest HttpMethod.GET
+
 let httpPost = makeRequest HttpMethod.POST
  
 let requestBudget (id, apiKey) =
-  httpGet (sprintf "budgets/%i/expanded" id) apiKey
-  |> Promise.map (Decode.fromString BudgetResponse.Decoder)
+    httpGet (sprintf "budgets/%i/expanded" id) apiKey
+    |> Promise.map (Decode.fromString BudgetResponse.Decoder)
--- src/Model.fs
+++ src/Model.fs
@@ -3,62 +3,62 @@ module Model
 open Thoth.Json
 
 type User = 
-  { Id: int64
-    Username: string
-    Email: string }
+    { Id: int64
+      Username: string
+      Email: string }
 
-  static member Decoder =
-    Decode.object (fun get ->
-      { Id = get.Required.Field "id" Decode.int64
-        Username = get.Required.Field "username" Decode.string
-        Email = get.Required.Field "email" Decode.string })
+    static member Decoder =
+        Decode.object (fun get ->
+          { Id = get.Required.Field "id" Decode.int64
+            Username = get.Required.Field "username" Decode.string
+            Email = get.Required.Field "email" Decode.string })
 
 type Item =
-  { Id: int64
-    Name: string
-    Price: decimal
-    Purchased: bool }
+    { Id: int64
+      Name: string
+      Price: decimal
+      Purchased: bool }
 
-  static member Decoder =
-    Decode.object (fun get ->
-      { Id = get.Required.Field "id" Decode.int64
-        Name = get.Required.Field "name" Decode.string
-        Price = get.Required.Field "price" Decode.decimal
-        Purchased = get.Required.Field "purchased" Decode.bool })
+    static member Decoder =
+        Decode.object (fun get ->
+            { Id = get.Required.Field "id" Decode.int64
+              Name = get.Required.Field "name" Decode.string
+              Price = get.Required.Field "price" Decode.decimal
+              Purchased = get.Required.Field "purchased" Decode.bool })
 
 type Recipient =
-  { Id: int64
-    Name: string
-    Amount: decimal
-    Items: seq<Item> }
+    { Id: int64
+      Name: string
+      Amount: decimal
+      Items: seq<Item> }
 
-  static member Decoder =    
-    Decode.object (fun get ->
-      { Id = get.Required.Field "id" Decode.int64
-        Name = get.Required.Field "name" Decode.string
-        Amount = get.Required.Field "amount" Decode.decimal
-        Items =
-          get.Optional.Field "items" (Decode.list Item.Decoder)
-          |> Option.defaultValue [] })
+    static member Decoder =    
+        Decode.object (fun get ->
+            { Id = get.Required.Field "id" Decode.int64
+              Name = get.Required.Field "name" Decode.string
+              Amount = get.Required.Field "amount" Decode.decimal
+              Items =
+                 get.Optional.Field "items" (Decode.list Item.Decoder)
+                 |> Option.defaultValue [] })
 
 type Budget =
-  { Id: int64
-    Name: string
-    Amount: decimal
-    Recipients: seq<Recipient> }
+    { Id: int64
+      Name: string
+      Amount: decimal
+      Recipients: seq<Recipient> }
   
-  static member Decoder =
-    Decode.object (fun get ->
-      { Id = get.Required.Field "id" Decode.int64
-        Name = get.Required.Field "name" Decode.string
-        Amount = get.Required.Field "amount" Decode.decimal
-        Recipients =
-          get.Optional.Field "recipients" (Decode.list Recipient.Decoder)
-          |> Option.defaultValue [] })
+    static member Decoder =
+        Decode.object (fun get ->
+            { Id = get.Required.Field "id" Decode.int64
+              Name = get.Required.Field "name" Decode.string
+              Amount = get.Required.Field "amount" Decode.decimal
+              Recipients =
+                  get.Optional.Field "recipients" (Decode.list Recipient.Decoder)
+                  |> Option.defaultValue [] })
 
 type BudgetResponse =
-  { Data: Budget }
+    { Data: Budget }
 
-  static member Decoder =
-    Decode.object (fun get ->
-      { Data = get.Required.Field "data" Budget.Decoder })
+    static member Decoder =
+        Decode.object (fun get ->
+            { Data = get.Required.Field "data" Budget.Decoder })
